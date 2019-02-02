86909e70feb11b294fa010829b23914dc2c1de65
Sara Torrey <sara.moravej@gmail.com>
2019-02-02 16:48:56 -0600

    Add new blog form page with persistence

--- src/main/java/com/saratorrey/blog/restApp/BlogController.java
+++ src/main/java/com/saratorrey/blog/restApp/BlogController.java
@@ -2,18 +2,22 @@ package com.saratorrey.blog.restApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.annotation.Resource;
 
+@SessionAttributes({"newBlog"})
 @Controller
 public class BlogController {
 
     @Resource
     BlogRepo blogRepo;
 
-    @RequestMapping( "/blogPost/{webTitle}" )
+    @GetMapping( "/blogPost/{webTitle}" )
     public String index( @PathVariable String webTitle, Model model ) {
 
         BlogPost blogPost = blogRepo.findOneByWebTitle( webTitle );
@@ -25,11 +29,33 @@ public class BlogController {
         return "index";
     }
 
-    @RequestMapping( "/blogList" )
+    @GetMapping( "/blogList" )
     public String blogList( Model model ) {
 
-        model.addAttribute( "blogList", blogRepo.findAll() );
+        model.addAttribute( "blogList", blogRepo.findAllByOrderByTitle() );
 
         return "blogList";
+    }
+
+    // Go to page
+    @GetMapping( "/addBlog" )
+    public String addBlog( Model model ) {
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setTitle( "New Post" );
+        blogPost.setWebTitle( "new-post" );
+        blogPost.setBody( "This is my beautiful body! woot woot!" );
+        model.addAttribute( "newBlog", blogPost );
+
+
+        return "addBlog";
+    }
+
+    @PostMapping( "/saveBlog" )
+    public String saveBlog(  @ModelAttribute("blogPost") BlogPost blogPost ,Model model ) {
+
+        blogRepo.save( blogPost );
+
+        return "redirect:/blogList";
     }
 }
--- src/main/java/com/saratorrey/blog/restApp/BlogRepo.java
+++ src/main/java/com/saratorrey/blog/restApp/BlogRepo.java
@@ -3,8 +3,11 @@ package com.saratorrey.blog.restApp;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface BlogRepo extends PagingAndSortingRepository<BlogPost, Long> {
 
     BlogPost findOneByWebTitle( String title );
+    List<BlogPost> findAllByOrderByTitle( );
 }
